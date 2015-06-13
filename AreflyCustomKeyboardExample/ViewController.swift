b81711b324a8dda0cdf64bf5d36e853b176e4354
Jason Ho <eflyjason@gmail.com>
2015-06-13 21:31:55 +0800

    Initial Commit

---
+++ AreflyCustomKeyboardExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AreflyCustomKeyboardExample
+//
+//  Created by Arefly on 13/6/2015.
+//  Copyright (c) 2015年 Arefly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
